9c9eaec70784c163c6f2f71582ac5369c993c8ec
ben-kaye <benkaye001@gmail.com>
2020-07-19 16:58:04 +0100

    fix formu, improve vis, fix crit err

--- constrained_mpc_ca.m
+++ constrained_mpc_ca.m
@@ -35,12 +35,14 @@ Bd = [   0.00370875152761323,    0;
 % initial conditions
 ref = [ 0.6; 0.2; 0; 0; 0; 0 ];
 x0 = [ -0.2; -0.5; 0; 0; 0; 0 ];
+x0 = [-0.196288558510390;-0.496291279506062;0.105783346431667;0.105705792729527;-0.193243865753017;0.193385643954741];
+
 obj_xy = [ -0.9; -0.9 ];
 min_dist = 0.1;
 
 % predictive horizon
 N = 10;
-static_states = 2;
+o_states = 2;
 
 % constraints
 umin = [ -1; -1 ];
@@ -55,42 +57,46 @@ QN = Q;
 R = 0.01 * eye(2);
 
 % Augmented Quadratic Objective
-P = blkdiag( kron(speye(N), Q ), QN, kron(speye(N), R), zeros(2));
+P = blkdiag( kron(speye(N), Q ), QN, zeros(o_states), kron(speye(N), R));
 % Augmented Linear Objective
-q = [ repmat( -Q*ref, N, 1 ); -QN*ref; zeros(N*nu, 1); zeros(2,1) ];
+q = [ repmat( -Q*ref, N, 1 ); -QN*ref; zeros(o_states,1); zeros(N*nu, 1)];
 % q = zeros( nx*(N+1) + N*nu, 1 );
 
 
 % Augmented Dynamics
 Ax = kron( speye(N+1), -speye(nx) ) +...
     kron( sparse(diag(ones(N,1), -1)), Ad);
-
 Bu = kron( [ sparse(1, N); speye(N) ], Bd );
-Aeq = [ Ax, Bu ];
 
 % append dynamics of object = stationary
-Aeq = blkdiag( Aeq, speye(2) );
+Ax = blkdiag(Ax, -speye(o_states));
+Bu = [Bu; zeros(o_states,size(Bu, 2))];
 
+Aeq = [ Ax, Bu ];
+
+% should it be -ve or +ve
 leq = [ -x0; zeros(N*nx, 1); -obj_xy ];
 ueq = leq;
 
 % input and state constraints
 
 % augmented state no.
-Nstates = (N+1)*nx + N*nu + 2;
+Nstates = (N+1)*nx + N*nu + o_states;
 
-Aineq = speye( (N+1)*nx + N*nu + 2 );
-Aineq = [ Aineq; zeros( N+1, Nstates )];
+Aineq = speye( Nstates );
 
-lineq = [ repmat( xmin, N+1, 1); repmat( umin, N, 1); -inf*ones((N+1)+2, 1) ];
-uineq = [ repmat( xmax, N+1, 1); repmat( umax, N, 1); inf*ones((N+1)+2, 1) ];
+lineq = [ repmat( xmin, N+1, 1); -inf*ones(o_states, 1); repmat( umin, N, 1)  ];
+uineq = [ repmat( xmax, N+1, 1); +inf*ones(o_states, 1); repmat( umax, N, 1) ];
 
 % [ Aineq, lineq ] = update_dist_constraint(Aineq, lineq, N, nx, nu, min_dist, [x0; zeros(Nstates - nx -nu -2, 1)]);
 
+
+Aplus = [ kron(speye(N+1), [ 1 1 0 0 0 0]), -ones(N+1, o_states), zeros(N+1, N*nu) ];
+
 % osqp constraints
-A = [ Aeq; Aineq ];
-l = [ leq; lineq ];
-u = [ ueq; uineq ];
+A = [ Aeq; Aineq; Aplus ];
+l = [ leq; lineq; -inf*ones((N+1), 1) ];
+u = [ ueq; uineq; inf*ones((N+1), 1)];
 
 % osqp init and setup
 prob = osqp;
@@ -103,7 +109,9 @@ hold on
 plot(ref(1), ref(2), 'bx', 'MarkerSize', 30)
 plot(obj_xy(1), obj_xy(2), 'k+')
 viscircles(obj_xy', min_dist)
-simtime = 100;
+
+
+simtime = 30;
 for i = 1 : simtime
     
     res = prob.solve();
@@ -112,39 +120,56 @@ for i = 1 : simtime
     end
     
     xN = res.x;
-    ctrl = xN( (N+1)*nx+1 : (N+1)*nx + nu );
+    ctrl = xN( (N+1)*nx + o_states + 1 : (N+1)*nx + o_states + nu );
+    
+    ctrls = xN( (N+1)*nx + o_states + 1 : end );
+    ctrls = reshape(ctrls, [nu, N]);
+    
     x0 = Ad * x0 + Bd * ctrl;
+    hold on
+    plot(x0(1),x0(2), 'r.', 'MarkerSize', 20)
+%     axis([-1 1 -1 1])
+    axis equal
+    
+    x1 = x0;
+    for j = 2:4
+        x1 = Ad * x1 + Bd * ctrls(:, j);
+        hold on
+        zscat = scatter(x1(1),x1(2), 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b');
+        zscat.MarkerFaceAlpha = .2;
+        zscat.MarkerEdgeAlpha = .2;
+        axis equal
+    end
     
     
-%     [ Aineq, lineq ] = update_dist_constraint(Aineq, lineq, N, nx, nu, min_dist, xN);
+    [ A, l ] = update_dist_constraint(A, l, N, nx, nu, o_states, min_dist, xN);
 %     A = [ Aeq; Aineq];
 %     l = [ leq; lineq];
     
     
     l(1:nx) = -x0;
     u(1:nx) = -x0;
-    prob.update('l',l, 'u',u, 'Ax',A);
+%     prob.update('l',l, 'u',u, 'Ax',A); updating A throws an error
+    prob.update('l',l, 'u',u);
+    prob.update('Ax', nonzeros(A));
     
-    hold on
-    plot(x0(1),x0(2), 'r.', 'MarkerSize', 20)
-%     axis([-1 1 -1 1])
-    axis equal
     
 end
 
-function [ Aineq, lineq ] = update_dist_constraint(Aineq, lineq, N, nx, nu, min_dist, x)
+function [ A, l ] = update_dist_constraint(A, l, N, nx, nu, no, min_dist, x)
    
 %     min_dist = 0.3;
+    [ m, n ] = size(A);
+    start_idx = m - N - 1; % i = start_idx + k
     
-    start_idx = (N+1)*nx + N*nu + 2; % i = start_idx + k
-    obj_xy = x(end-1:end);
+    obj_xy = x((N+1)*nx + 1:(N+1)*nx + no);
     
 %     Aineq = blkdiag(speye( (N+1)*nx + N*nu ), zeros(2));
-    % add in N+1 object dist constraints
-    Aineq(start_idx+1:end,:) = zeros(N+1, start_idx);
+    % reset last N+1 constraints
+    A(end-N:end,:) = zeros(N+1, n);
     
 %     lineq = [ repmat( xmin, N+1, 1); repmat( umin, N, 1); zeros(2,1) ];
-    lineq(start_idx+1:end) = min_dist * ones( N+1, 1 );
+    l(end-N:end) = min_dist * ones( N+1, 1 );
    
 %     uineq = [ repmat( xmax, N+1, 1); repmat( umax, N, 1); zeros(2,1) ];
 %     uineq = [ unieq; inf * ones(N+1, 1) ];
@@ -159,7 +184,7 @@ function [ Aineq, lineq ] = update_dist_constraint(Aineq, lineq, N, nx, nu, min_
         
         
         T = eta' * [ eye(2), -eye(2) ];
-        Aineq( start_idx + k, [ (k-1)*nx + 1 : (k-1)*nx + 2, end-1 : end ] ) = T;
+        A( start_idx + k, [ (k-1)*nx + 1 : (k-1)*nx + 2, (N+1)*nx + 1 : (N+1)*nx + no ] ) = T;
 %         lineq( start_idx + k ) = lineq( start_idx + k ) - x_norm; % + 0
 %         lineq( start_idx + k ) = lineq( start_idx + k );
     end
