20520eeea062b66b50b150ee77bed9bec65dc7ae
ben-kaye <benkaye001@gmail.com>
2020-08-17 23:14:24 +0100

    update

--- CBF-QP/drone_cbf_ca.m
+++ CBF-QP/drone_cbf_ca.m
@@ -1,41 +1,37 @@
-%$sign
-%Ben Kaye, (c) 2020
-%Sequential QP with RCBF constraint for CA
-%$br
-%Using OSQP solver
-%github.com/oxfordcontrol/osqp
-%$endsign
+% * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+% *                                                                     *
+% *                      Ben Kaye, (c) 2020                             *
+% *           Sequential QP with RCBF constraint for CA                 *
+% *                                                                     *
+% * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+% *                                                                     *
+% *                       Using OSQP solver                             *
+% *                 github.com/oxfordcontrol/osqp                       *
+% *                                                                     *
+% * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+clear
 
 %%% PARAMETERS %%%
 
-Ad = [      1,  0,  0.0962912484723868,     0,                      0,                      0.0396218015830554;
-            0,  1,  0,                      0.0962912484723868,     -0.0396218015830554,    0;
-            0,  0,  0.894293322749997,      0,                      0,                      0.702694931894877;
-            0,  0,  0,                      0.894293322749997,      -0.702694931894877,     0;
-            0,  0,  0,                      0.193245482770890,      0.452393730892869,      0;
-            0,  0,  -0.193245482770890,     0,                      0,                      0.452393730892869   
-];
 
+T_controller = 1e-3;
+Ad = [1,0,0.000999995725421902,0,0,4.89519300035470e-06;0,1,0,0.000999995725421901,-4.89519300035470e-06,0;0,0,0.999987182106712,0,0,0.00978058201074243;0,0,0,0.999987182106712,-0.00978058201074243,0;0,0,0,0.00261084855543965,0.994003677759774,0;0,0,-0.00261084855543965,0,0,0.994003677759774];
 % d_system.B([1:2, 4:5, 7:8], [4, 5])
-Bd =    [   0.00370875152761323,    0;
-            0,                      0.00370875152761323;
-            0.105706677250003,      0;
-            0,                      0.105706677250003;
-            0,                      -0.193245482770890;
-            0.193245482770890,      0                       
-];
+Bd = [4.27457809838634e-09,0;0,4.27457809838634e-09;1.28178932883944e-05,0;0,1.28178932883944e-05;0,-0.00261084855543965;0.00261084855543965,0];
 
 
-Au = Ad([1,2], :);
-Bu = Bd([1,2], :);
+Au = Ad([3,4], :);
+Bu = Bd([3,4], :);
 
-p_o = [ 1; 1 ];
+p_o = [ 1.2; 1 ];
 
 gamma = 1; % {}
 g = 9.81; % {ms-2}
-a_max = 0.2 * g; % {ms-2} Maximum decceleration allowed (design parameter)
+a_max = 1 * g; % {ms-2} Maximum decceleration allowed (design parameter)
 delta = 0.3; % {m} Distance constraint
-T = 1e-3; % {s} Simulation step size
+% T = 1e-3; % {s} Simulation step size
+T = T_controller; % MUST BE 0.1s as dynamic system discretised to
 sim_time = 10; % {s}
 
 
@@ -44,8 +40,8 @@ N = floor(sim_time/T);
 p0 = [ 0; 0 ];
 x_r = [ 2; 2; 0; 0; 0; 0 ];
 
-Q = [ 1; 1; 0; 0; 0; 0 ];
-R = [ 1; 1 ];
+Q = diag([ 1e10; 1e10; 0; 0; 0; 0 ]);
+R = diag([ 1; 1 ]);
 
 P = blkdiag(Q,R);
 
@@ -57,30 +53,38 @@ Aeq = [ eye(6), -Bd ];
 leq = Ad*x;
 ueq = leq;
 
-[ Acbf, ucbf ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma);
+[ Acbf, ucbf, ~ ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma);
 lcbf = -inf;
+
+Acc = [ zeros(2,6), eye(2) ];
+lcc = -1 *  ones(2,1);
+ucc = ones(2,1);
+
 
 %%% SOLVER SETUP %%%
 
-A = [ Aeq; Acbf ];
-u = [ ueq; ucbf ];
-l = [ leq; lcbf ];
+A = [ Aeq; Acbf; Acc ];
+u = [ ueq; ucbf; ucc ];
+l = [ leq; lcbf; lcc ];
 
 solver = osqp;
 
-solver.setup(P, q, A, l, u);
+solver.setup(P, q, A, l, u, 'warm_start',true,'verbose',false);
 
 %%% SIMULATION %%%
 
 errors = 0;
 u_act = [ 0; 0 ];
+p_t = zeros(2,N);
+h_t = zeros(1,N);
 
 for e = 1:N
 
     res = solver.solve();
-    if res.info.status ~= -1
+    if res.info.status_val ~=1
         %%% INFEASIBILITY %%%
         errors = errors + 1;
+        break
     else
         %%% SOLVED %%%
         x_star = res.x;
@@ -89,21 +93,36 @@ for e = 1:N
 
     %%% SIMULATE TIME STEP %%%
     x = Ad*x + Bd * u_act;
+    p_t(:,e) = x([1,2]);
 
     %%% UPDATE SOLVER %%%
     leq = Ad*x;
-    [ Acbf, ucbf ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma);
+    ueq = leq;
+    [ Acbf, ucbf, h ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma);
+    h_t(e) = h;
 
-    A = [ Aeq; Acbf ];
-    u = [ ueq; ucbf ];
-    l = [ leq; lcbf ];
+    A = [ Aeq; Acbf; Acc ];
+    u = [ ueq; ucbf; ucc ];
+    l = [ leq; lcbf; lcc ];
+
 
     solver.update('Ax', nonzeros(A), 'u', u, 'l', l)
 end
 
+%%% PLOTTING %%%
+t = 1:T:N;
+t = t(1:e);
+p_t = p_t(:,1:e);
+h_t = h_t(1:e);
+
+plot(p_t(1,:), p_t(2,:), '.');
+viscircles(p_o', delta);
+
+
+
 %%% FUNCTIONS %%%
 
-function [ Acbf, ucbf ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma)
+function [ Acbf, ucbf, h ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma)
 %Reciprocal Control Barrier Function constraint
     p = x([1, 2]);
     pdot = x([3, 4]);
@@ -113,7 +132,7 @@ function [ Acbf, ucbf ] = getCBF(Au, Bu, x, p_o, a_max, delta, gamma)
 
     Bf = -log(h/(1+h));
 
-    coeff = 2*(p_L'*pdot)/a_max*( delta + (p_L'*pdot)^2/2/a_max ) 
+    coeff = 2*(p_L'*pdot)/a_max*( delta + (p_L'*pdot)^2/2/a_max );
     LfBF = 1/h/(1+h)*( 2*(p_L'*pdot) + coeff*( p_L'*Au*x - (pdot'*pdot) ) );
     LgBF = 1/h/(1+h)*coeff*( p_L'*Bu );
 
