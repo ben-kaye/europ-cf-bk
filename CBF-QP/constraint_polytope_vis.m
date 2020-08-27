005fe0bf75bf7fe2120bca0ad55288b5d525633a
ben-kaye <benkaye001@gmail.com>
2020-08-27 14:00:14 +0100

    improve debug util

--- CBF-QP/constraint_polytope_vis.m
+++ CBF-QP/constraint_polytope_vis.m
@@ -1,11 +1,15 @@
-v = -1:0.01:13;  % plotting range from -5 to 5
-[v, omega] = meshgrid(v);  % get 2-D mesh for x and y
-conditions = (A(1,1)*v + A(1,2)*omega < u(1)) & (A(2,1)*v + A(2,2)*omega < u(2)) ...
-    & (A(2,1)*v + A(2,2)*omega > l(2)) & (A(3,1)*v + A(3,2)*omega < u(3)) ...
-    & (A(3,1)*v + A(3,2)*omega > l(3));
-cond = zeros(length(v)); % Initialize
-cond(conditions) = NaN;
-surf(v, omega, cond)
+range = -2.5:0.01:13;  % plotting range
+[v_ineq, omega_ineq] = meshgrid(range);  % get 2-D mesh for v and omega
+
+cond = ones(length(range)); % Initialize
+for it = 1:size(A,1)
+    cond_i = (A(it,1)*v_ineq + A(it,2)*omega_ineq < u(it)) & (A(it,1)*v_ineq + A(it,2)*omega_ineq > l(it));
+    cond = cond & cond_i;
+end
+
+polytope = zeros(length(range)); % Initialize
+polytope(cond) = NaN;
+surf(v_ineq, omega_ineq, polytope)
 xlabel('v')
 ylabel('\omega')
 view(0,90)
