6a21cd718c33a10e8e40a7e87d1cb1e43edda3ab
ben-kaye <benkaye001@gmail.com>
2020-09-04 01:55:20 +0100

    update

--- safety_func_vis.m
+++ safety_func_vis.m
@@ -1,10 +1,11 @@
 step = 5e-2;
 eps_range = 0:step:3;
-beta_range = 0:step:pi;
+beta_range = -pi:step:pi;
 
 [ beta, epsilon ] = meshgrid(beta_range, eps_range);
 
 h = epsilon.^2 .* (1 + sin(beta).^2) - 2;
+h2 = epsilon.^2 .* (1 + cos(beta/2)) - 2;
 
 surf(beta_range, eps_range, h)
 xlabel('\beta');
