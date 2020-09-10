e18b517395c02e691ee92893a7259c92b875c9dc
ben-kaye <benkaye001@gmail.com>
2020-09-10 23:40:33 +0100

    up

--- Controllers/init_graphic.m
+++ Controllers/init_graphic.m
@@ -1,6 +1,6 @@
 function rotors = init_graphic(offset, radii, N_sides)    
     beta = 0:2*pi/N_sides:2*pi;
-    p = radii*[ cos(beta); sin(beta) ];
+    p = radii*[ cos(beta); sin(beta) ]; %
     
     rotors = [ p + offset; p + [ offset; -offset ]; p - offset; p + [ -offset; offset ] ];
 end
